b13faec77d078c3368c243f9971bda9c2d159792
Clément Demongeot <clement.demongeot.art@gmail.com>
2024-06-28 15:35:20 +0200

    pages1

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { ghPages } from "vite-plugin-gh-pages";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: "/Projet_Argent_Bank/",
+  base: "https://clementdemongeot.github.io/Projet_Argent_Bank/",
 });
